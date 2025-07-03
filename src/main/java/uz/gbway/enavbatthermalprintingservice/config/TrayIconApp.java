7460a9464c47c7a298a396762b0af577dffad865
Jayxunbey <muxammedovjayxun@gmail.com>
2025-07-03 18:50:02 +0500

    implemented icon

--- src/main/java/uz/gbway/enavbatthermalprintingservice/config/TrayIconApp.java
+++ src/main/java/uz/gbway/enavbatthermalprintingservice/config/TrayIconApp.java
@@ -1,7 +1,11 @@
 package uz.gbway.enavbatthermalprintingservice.config;
 
+import uz.gbway.enavbatthermalprintingservice.ENavbatThermalPrintingServiceApplication;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class TrayIconApp {
@@ -24,16 +28,44 @@ public class TrayIconApp {
         Image image = ImageIO.read(iconStream);
 
         PopupMenu popup = new PopupMenu();
+
+        // 🔁 Restart menyusi
+        MenuItem restartItem = new MenuItem("Restart");
+        restartItem.addActionListener(e -> {
+            try {
+                restartApplication();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
         MenuItem exitItem = new MenuItem("Chiqish");
         exitItem.addActionListener(e -> System.exit(0));
+
+        popup.add(restartItem);
         popup.add(exitItem);
 
-        TrayIcon trayIcon = new TrayIcon(image, "Mening Ilovam", popup);
+        TrayIcon trayIcon = new TrayIcon(image, "GB Way Printing Service", popup);
         trayIcon.setImageAutoSize(true);
         SystemTray.getSystemTray().add(trayIcon);
 
         System.out.println("Tray supported!"); // keyingi kodlarni shu yerga qo‘shasiz
 
+    }
+
+    // 🔄 Dastur qayta ishga tushirish metodi
+    private static void restartApplication() throws IOException {
+        String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+        File currentJar = new File(ENavbatThermalPrintingServiceApplication.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+
+        if (!currentJar.getName().endsWith(".jar")) {
+            System.out.println("JAR fayl emas. Qayta ishga tushirish bekor qilindi.");
+            return;
+        }
+
+        final String[] command = new String[] { javaBin, "-jar", currentJar.getPath() };
+
+        new ProcessBuilder(command).start();
+        System.exit(0); // eski jar ni yopamiz
     }
 
 }
